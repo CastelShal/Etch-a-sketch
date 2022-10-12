d715d9da96ca38a8d2e380a219122a48686be3ce
CastelShal <castelshal@gmail.com>
2022-10-12 20:13:32 +0530

    Add buttons for interactivity and rendering functions

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 const wrapper = document.querySelector('div.wrapper');
-let gridsize = prompt("Enter grid size")
-gridsize = Math.min(gridsize,100)
-renderGrid(gridsize)
+const clearButton = document.getElementById('clear-canvas');
+const sizeButton = document.getElementById('get-size');
+let gridSize = 16;
+renderGrid(gridSize);
 
-console.log(wrapper.textContent);
+clearButton.onclick = () => renderGrid(gridSize);
+sizeButton.onclick = getSize;
 
-function changeColor(ev){
+function changeColor(){
     this.style.backgroundColor = 'blue';
 }
-function renderGrid(size=16){
-    wrapper.replaceChildren()
-    for(i=0;i<size;i++){
+
+function renderGrid(){
+    wrapper.replaceChildren();       //clears the wrapper grid
+    for(i=0;i<gridSize;i++){
         let flexdiv = document.createElement('div');
-        flexdiv.classList.add("flex-div")
+        flexdiv.classList.add("flex-div");
         wrapper.appendChild(flexdiv);
 
-        for(let j=0;j<size;j++){
-            let elem = document.createElement('div')
-            elem.classList.add('cell')
+        for(let j=0;j<gridSize;j++){
+            let elem = document.createElement('div');
+            elem.classList.add('cell');
             elem.onmouseover = changeColor;
-            flexdiv.appendChild(elem)
+            flexdiv.appendChild(elem);
         }
     }
+}
+
+function getSize(){
+    gridSize = prompt("Enter size of required grid:");
+    renderGrid();
 }
