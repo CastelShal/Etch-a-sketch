db55f9a362bf54b8e70820c6d56bbd14ad70f091
CastelShal <castelshal@gmail.com>
2022-10-25 18:08:08 +0530

    Completed with GUI and features

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
 const wrapper = document.querySelector('div.wrapper');
 const clearButton = document.getElementById('clear-canvas');
 const sizeButton = document.getElementById('get-size');
+const normal = document.getElementById('nm-mode');
+const rbow = document.getElementById('rb-mode');
+const display = document.querySelector('.display');
 let gridSize = 16;
-renderGrid(gridSize);
+display.textContent = `${gridSize} x ${gridSize}`;
+rbg = false;
+normal.style.backgroundColor = 'green';
 
 clearButton.onclick = () => renderGrid(gridSize);
 sizeButton.onclick = getSize;
+normal.onclick = ev=>{
+    rbg = false;
+    normal.style.backgroundColor = 'green';
+    rbow.style.removeProperty('background-color');
+}
+rbow.onclick = ev =>{
+    rbg = true;
+    rbow.style.backgroundColor = 'green';
+    normal.style.removeProperty('background-color');
+}
+
+renderGrid();
 
 function changeColor(){
-    this.style.backgroundColor = 'blue';
+    if(rbg){
+        const randoColors = [];
+        for(i=0;i<3;i++){
+            randoColors[i] = Math.floor(Math.random() * 256);
+        }
+        this.style.backgroundColor = `rgb(${randoColors[0]},${randoColors[1]},${randoColors[2]}`;
+    }
+    else{
+        this.style.backgroundColor = 'blue';
+    }
+
 }
 
 function renderGrid(){
     wrapper.replaceChildren();       //clears the wrapper grid
+    display.textContent = `${gridSize} x ${gridSize}`;
     for(i=0;i<gridSize;i++){
         let flexdiv = document.createElement('div');
         flexdiv.classList.add("flex-div");
@@ -21,6 +49,7 @@ function renderGrid(){
         for(let j=0;j<gridSize;j++){
             let elem = document.createElement('div');
             elem.classList.add('cell');
+            elem.setAttribute('data-id',0);
             elem.onmouseover = changeColor;
             flexdiv.appendChild(elem);
         }
